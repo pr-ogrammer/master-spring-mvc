71bcb12d6534e01adfde5de24de467cacc45967b
pr-ogrammer <rutkowski1988@gmail.com>
2016-10-11 06:24:03 +0200

    Working with lists

--- src/main/java/pl/prutkowski/master/spring/mvc/controller/profile/ProfileController.java
+++ src/main/java/pl/prutkowski/master/spring/mvc/controller/profile/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.prutkowski.master.spring.mvc.date.USLocalDateFormatter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
@@ -28,11 +29,24 @@ public class ProfileController {
         return PROFILE_FORM_VIEW;
     }
 
-    @PostMapping("/profile")
+    @PostMapping(value = "/profile", params = {"save"})
     public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return PROFILE_FORM_VIEW;
         }
         return "redirect:/profile";
+    }
+
+    @PostMapping(value = "/profile", params = {"addTaste"})
+    public String addRow(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+        profileForm.getTastes().add(null);
+        return PROFILE_FORM_VIEW;
+    }
+
+    @PostMapping(value = "/profile", params = {"removeTaste"})
+    public String removeRow(@Valid ProfileForm profileForm, BindingResult bindingResult, HttpServletRequest request) {
+        Integer rowId = Integer.valueOf(request.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return PROFILE_FORM_VIEW;
     }
 }
