99a789c2f969570316284b8fdd042fcd54dbca64
pr-ogrammer <rutkowski1988@gmail.com>
2016-11-03 05:32:39 +0100

    Spring Security configuration

--- src/main/java/pl/prutkowski/master/spring/mvc/config/ApiSecurityConfiguration.java
+++ src/main/java/pl/prutkowski/master/spring/mvc/config/ApiSecurityConfiguration.java
@@ -2,37 +2,36 @@ package pl.prutkowski.master.spring.mvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * Created by programmer on 11/1/16.
+ * Created by programmer on 11/2/16.
  */
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Order(1)
+public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-            .withUser("user").password("user").roles("USER").and()
-            .withUser("admin").password("admin").roles("USER", "ADMIN");
+                .withUser("user").password("user").roles("USER").and()
+                .withUser("admin").password("admin").roles("USER", "ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
-                .and()
+        http.antMatcher("/api/**")
+                .httpBasic().and()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "/logout").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/**").hasRole("USER")
-                .antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET).hasRole("USER")
+                .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
                 .anyRequest().authenticated();
     }
 }
---
+++ src/main/java/pl/prutkowski/master/spring/mvc/config/WebSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package pl.prutkowski.master.spring.mvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by programmer on 11/2/16.
+ */
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .defaultSuccessUrl("/profile").and()
+                .logout().logoutSuccessUrl("/login").and()
+                .authorizeRequests()
+                .antMatchers("/webjars/**", "/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}
