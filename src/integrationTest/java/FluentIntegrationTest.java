9e370ea9ad685b5f14e2d989025e74ae515d3494
pr-ogrammer <rutkowski1988@gmail.com>
2016-12-18 14:26:20 +0100

    Integration tests

--- src/integrationTest/java/FluentIntegrationTest.java
+++ src/integrationTest/java/FluentIntegrationTest.java
@@ -1,4 +1,5 @@
 import org.fluentlenium.adapter.FluentTest;
+import org.fluentlenium.core.annotation.Page;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
@@ -6,7 +7,11 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pages.LoginPage;
+import pages.ProfilePage;
+import pages.SearchResultPage;
 import pl.prutkowski.master.spring.mvc.MasterSpringMvcApplication;
+import pl.prutkowski.master.spring.mvc.auth.StubSocialSigninConfig;
 import pl.prutkowski.master.spring.mvc.controller.search.StubTweeterSearchConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,11 +24,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(
         classes = {
                 MasterSpringMvcApplication.class,
-                StubTweeterSearchConfig.class
+                StubTweeterSearchConfig.class,
+                StubSocialSigninConfig.class
         },
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
 public class FluentIntegrationTest extends FluentTest {
+
+    @Page
+    private LoginPage loginPage;
+    @Page
+    private ProfilePage profilePage;
+    @Page
+    private SearchResultPage searchResultPage;
 
     @Value("${local.server.port}")
     private int serverPort;
@@ -42,5 +55,19 @@ public class FluentIntegrationTest extends FluentTest {
     public void hasPageTitle() {
         goTo("/");
         assertThat(findFirst("h2").getText()).isEqualTo("Login page");
+    }
+
+    @Test
+    public void shouldBeRedirectedAfterFillingForm() {
+        goTo("/");
+        loginPage.isAt();
+        loginPage.login();
+        profilePage.isAt();
+        profilePage.fillInfos("programmer", "programmer@email.com", "03/19/1985");
+        profilePage.addTaste("spring");
+        profilePage.saveProfile();
+////        takeScreenShot();
+        searchResultPage.isAt("spring");
+        assertThat(searchResultPage.getNumberOfResults()).isEqualTo(2);
     }
 }
